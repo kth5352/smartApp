65672a4f79113ec32fd273271326e6207e3a47a3
kth5352 <kth5352@gmail.com>
2023-12-18 09:40:00 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express"
+import mysql from "mysql"
+import bodyParser from "body-parser"
+import cors from "cors"
+
+const app = express()
+const port = 3010
+
+const db = mysql.createConnection({
+    host: 'svc.sel5.cloudtype.app',
+    user: 'root',
+    password: '1234',
+    database: 'vdb'
+})
+
+db.connect()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json({ result: "success" })
+})
+
+
+app.get('/vtuber-data', (req, res) => {
+    const sql = 'SELECT * from vtuber'
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.json({ result: "error" })
+            return console.log(err)
+        }
+        res.json(rows)
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`서버 실행됨 (port ${port})`);
+});
